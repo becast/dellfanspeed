bce54c5d48e4dd0a78df49e4daa3e2024bf6cb8b
Genuineparts <itsme@genuineparts.org>
2022-05-14 18:05:49 +0200

    Java version update

--- src/main/java/at/becast/dellfancontroller/dellfanspeed.java
+++ src/main/java/at/becast/dellfancontroller/dellfanspeed.java
@@ -7,26 +7,23 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class dellfanspeed{
-    private static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private static final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
     static ipmitool ipmi = new ipmitool();
     static Sensors temp = new Sensors();
-    private static Checker check = new Checker();
-    private static Settings config = Settings.getInstance();
+    private static final Checker check = new Checker();
+    private static final Settings config = Settings.getInstance();
     public static void main(String[] args) {
         //Main Code
         final Thread mainThread = Thread.currentThread();
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                try {
-                    System.out.println("Shutting Down, resuming automatic control");
-                    ipmi.switchmode(true);
-                    mainThread.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                System.out.println("Shutting Down, resuming automatic control");
+                ipmi.switchmode(true);
+                mainThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        });
+        }));
         executor.scheduleAtFixedRate(check, 0, config.getInt("dellfanspeed.refresh"), TimeUnit.SECONDS);
     }
 }
